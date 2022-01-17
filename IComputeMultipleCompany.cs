c935e69f2ebfe70dfc25567c798d996ace1f8b4e
Akhil <akhilu5042@gmail.com>
2022-01-17 08:57:03 +0530

    UC-10

---
+++ IComputeMultipleCompany.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputationProblem
+{
+    public interface IComputeMultipleCompany
+    {
+        public void addCompanyEmpWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth);
+
+        public void ComputeMultipleCompany();
+        public int getTotalWage(string company);
+    }
+}
